a58c8ccaabf603242db62a2515a8509b260078f4
lm <lm@1234998.cn>
2024-06-13 15:48:05 +0800

    适配667客户1280x800屏

--- app/src/main/java/com/android/launcher2/ResourceUtil.java
+++ app/src/main/java/com/android/launcher2/ResourceUtil.java
@@ -110,213 +110,37 @@ public class ResourceUtil {
         Utilities.mSystemUI = value;
         MMLog.d(TAG, "Utilities.mIsDSP=" + Utilities.mIsDSP);
 
-
 		//int sw = 0;
         ///int w = 0;
         ///int h = 0;
-        int type = 0; // deault 800X480
+        ///int type = 0; // deault 800X480
 
         DisplayManager displayManager = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
         Display[] display = displayManager.getDisplays();
         Configuration configuration = context.getResources().getConfiguration();
-        MMLog.d(TAG, "Display[]=" + Arrays.toString(display));
-		//Rect outRect = new Rect();
-		//display[0].getOverscanInsets(outRect);
-
         DisplayMetrics dm = context.getResources().getDisplayMetrics();
         mScreenWidth = dm.widthPixels;
         mScreenHeight = dm.heightPixels;
 
+        MMLog.d(TAG,"Display[]=" + Arrays.toString(display));
         MMLog.d(TAG,"DisplayMetrics=" + dm.toString());
-        MMLog.d(TAG,"configuration:"+configuration.toString());
-
+        MMLog.d(TAG,"Configuration1:"+configuration.toString());
 
         if (dm.widthPixels == 1024 && dm.heightPixels == 600) {//667
-            type = 1;
             sw = 321;
         } else if (dm.widthPixels == 1280 && dm.heightPixels == 480) {
-            type = 2;
             sw = 320;
         } else if (dm.widthPixels == 1280 && dm.heightPixels == 720) {//667
-            type = 3;
             sw = 321;
         } else if (dm.widthPixels == 1280 && dm.heightPixels == 800) {
-            type = 3;
             sw = 321;
         } else {
-            type = 0;
             sw = 320;
         }
 
-        if (MachineConfig.VALUE_SYSTEM_UI_KLD1.equals(value)) {
-            if (type == 0) {
-                sw = 322;
-                // w = 801;
-                // h = 479;
-            } else {
-                sw = 323;
-                // w = 1025;
-                // h = 601;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD2.equals(value) ||
-                MachineConfig.VALUE_SYSTEM_UI_KLD15_6413.equals(value) ||
-                MachineConfig.VALUE_SYSTEM_UI24_616.equals(value)) {
-            if (type == 0) {
-                sw = 324;
-            } else if (type == 2) {
-                sw = 324;
-            } else {
-                sw = 325;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD3.equals(value)) {
-            if (type == 0) {
-                sw = 326;
-            } else {
-                sw = 327;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD5.equals(value)) {
-            if (type == 0) {
-                sw = 328;
-            } else {
-                sw = 329;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD7_1992.equals(value)) {
-            if (type == 0) {
-                sw = 330;
-            } else {
-                sw = 331;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD3_8702.equals(value)
-                || MachineConfig.VALUE_SYSTEM_UI45_8702_2.equals(value)) {
-            if (type == 0) {
-                sw = 332;
-            } else {
-                sw = 333;
-            }
-
-			///if (MachineConfig.VALUE_SYSTEM_UI45_8702_2.equals(value)) {
-			///	h = 441;
-			///}
-        } else if (MachineConfig.VALUE_SYSTEM_UI_KLD10_887.equals(value)) {
-            if (type == 0) {
-                sw = 334;
-            } else if (type == 2) {
-                sw = 335;
-            } else {
-                sw = 336;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI20_RM10_1.equals(value)) {
-            if (type == 0) {
-                sw = 338;
-            } else if (type == 2) {
-                sw = 340;
-            } else {
-                sw = 339;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI21_RM10_2.equals(value)) {
-            if (type == 0) {
-                sw = 344;
-            } else if (type == 2) {
-                sw = 346;
-            } else {
-                sw = 345;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI16_7099.equals(value)) {
-            if (type == 0) {
-                sw = 348;
-            } else {
-                sw = 349;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI22_1050.equals(value)) {
-            if (type == 0) {
-                sw = 352;
-            } else {
-                sw = 353;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI31_KLD7.equals(value)) {
-            if (type == 0) {
-                sw = 356;
-            } else {
-                sw = 357;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI35_KLD813_2.equals(value)) {
-            if (type == 0) {
-                sw = 360;    //800x480
-            } else {
-                sw = 361;    //1024x600
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI21_RM12.equals(value)) {
-            if (type == 0) {
-                sw = 362;    //800x480
-            } else {
-                sw = 363;    //1024x600
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI40_KLD90.equals(value) ||
-                MachineConfig.VALUE_SYSTEM_UI_9813.equals(value)) {
-            if (type == 0) {
-                sw = 364; // 800x480
-            } else if (type == 2) {
-                sw = 366; // 1280x480
-            } else if (type == 3) {
-                sw = 367; // 1280x720
-            } else {
-                sw = 365; // 1024x600
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI41_2007.equals(value)) {
-            if (type == 0) {
-                sw = 372;    //800x480
-            } else {
-                sw = 373;    //1024x600
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI42_913.equals(value)
-                || MachineConfig.VALUE_SYSTEM_UI42_13.equals(value)) {
-
-            if (type == 0 || type == 2) {
-                sw = 368;    //800x480
-            } else {
-                sw = 369;    //1024x600
-            }
-
-            ///if (MachineConfig.VALUE_SYSTEM_UI42_13.equals(value)) {
-            ///   h = 450;
-            ///}
-
-        } else if (MachineConfig.VALUE_SYSTEM_UI44_KLD007.equals(value)) {
-            if (type == 0) {
-                sw = 380;    //800x480
-            } else {
-                sw = 381;    //1024x600
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_887_90.equals(value)) {
-            if (type == 0) {
-                sw = 386;
-            } else if (type == 2) {
-                sw = 387;
-            } else {
-                sw = 388;
-            }
-        } else if (MachineConfig.VALUE_SYSTEM_UI_PX30_1.equals(value)) {
-            if (type == 0) {
-                sw = 400;
-            } else if (type == 2) {
-                sw = 401;
-            } else {
-                sw = 402;
-            }
-        }
-
-
         configuration.smallestScreenWidthDp = sw;
-        ///if (w != 0) {
-        ///    c.screenWidthDp = w;
-        ///}
-        ///if (h != 0) {
-        ///    c.screenHeightDp = h;
-        ///}
-
-        ///MMLog.d(TAG, "w=" + w + ",h=" + h);
         context.getResources().updateConfiguration(configuration, null);
-        MMLog.d(TAG, "sw:" + sw + ",configuration:" + configuration.toString());
+        MMLog.d(TAG,"Configuration2:" + configuration.toString());
         return value;
     }
 
